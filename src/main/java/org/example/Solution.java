f07d5275d51ddfa76b603932c596077a01ca8c05
FlaviusGavriluta <flavius.gavriluta@gmail.com>
2023-08-13 12:26:10 +0300

    print the solution

---
+++ src/main/java/org/example/Solution.java
@@ -0,0 +1,15 @@
+package org.example;
+
+/*
+We solemnly publish and declare these variables...
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 3;
+        int sum = a + b;
+        System.out.println("a + b = " + sum);
+    }
+}
